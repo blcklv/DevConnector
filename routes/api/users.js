6278fbb35bd7df8d009b7c02f161fb53c17447be
Malik Snowden <malik@Maliks-MacBook-Pro.local>
2021-05-17 16:04:33 -0400

    creating user profiles

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 // @route    POST api/users
@@ -56,8 +58,22 @@ async (req, res) => {
 
         // Return jsonwebtoken
 
-        res.send('User Registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
         
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+        );
+
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error');
